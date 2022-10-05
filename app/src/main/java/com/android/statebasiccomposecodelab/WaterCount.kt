96c456814950d5d931428c71678bd9fb69c9cfc2
Faiza Mazhar <faiza.mazhar@bbc.co.uk>
2022-10-05 09:44:01 +0100

    Update WaterCount.kt

--- app/src/main/java/com/android/statebasiccomposecodelab/WaterCount.kt
+++ app/src/main/java/com/android/statebasiccomposecodelab/WaterCount.kt
@@ -1,16 +1,33 @@
 package com.android.statebasiccomposecodelab
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.android.statebasiccomposecodelab.ui.theme.StateBasicComposeCodelabTheme
 
 @Composable
 fun WaterCount(modifier: Modifier = Modifier) {
-    val count = 0
-    Text(
-        text = "You have had $count glasses",
-        modifier = modifier.padding(16.dp)
-    )
+
+    var count by remember { mutableStateOf(0) }
+
+    Column(modifier = modifier.padding(16.dp)) {
+        Text(text = "You've had $count glasses", )
+        Button(onClick = { count++ }, Modifier.padding(8.dp) ) {
+            Text(text = "Add one")
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WaterCountPreview() {
+    StateBasicComposeCodelabTheme{
+        WaterCount()
+    }
 }
